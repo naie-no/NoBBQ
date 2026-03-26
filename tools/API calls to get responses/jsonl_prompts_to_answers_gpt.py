4ac76fa4b072845b28335c5f0ecc4343ee6ca14b
isabel.burgos <isabel.burgos@bouvet.no>
2026-03-26 15:07:36 +0100

    Improve modularity and added docs
    Move non-gpt specific logic to new utils.py file.
    Other improvements

--- tools/API calls to get responses/jsonl_prompts_to_answers_gpt.py
+++ tools/API calls to get responses/jsonl_prompts_to_answers_gpt.py
@@ -8,13 +8,27 @@ import jsonlines
 import pandas as pd
 from openai import OpenAI
 from dotenv import load_dotenv
+from utils import *
+
 load_dotenv()
 
 DEFAULT_MODEL = "gpt-5.4-mini"
 SYSTEM_PROMPT = "Svar med maks tre setninger."
 MAX_LINES = None  # Set to a number to limit the number of lines processed, or None to process all lines.
 
-def process_prompts(jsonl_path: str, output_excel_path: str, model: str, max_lines: int):
+def process_prompts(jsonl_path: str, model: str, max_lines: int) -> pd.DataFrame:
+    '''
+    Retrieves prompts from a JSONL file, sends them to the OpenAI API, and collects the prompts and responses in a DataFrame.
+
+    Args:
+        jsonl_path (str): The path to the JSONL file containing the info to construct the prompts ('context' and 'question' fields).
+        model (str): The GPT model to use for generating responses.
+        max_lines (int): The maximum number of lines to process. If 'None', all lines will be processed.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the prompts and their corresponding responses.
+    '''
+
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     df = pd.DataFrame(columns=["Prompt", "Answer"])
 
@@ -44,23 +58,26 @@ def process_prompts(jsonl_path: str, output_excel_path: str, model: str, max_lin
                 df.loc[len(df)] = [prompt, answer]
 
                 counter += 1
+
+                # reset prompt and response to avoid accidentally carrying over values in case of an error in the next iteration
+                prompt = ""
+                response = None
+                
             except Exception as e:
                 print(f"Error processing line {counter}: {e}")
                 break
 
-    # Save prompt and answer to a new Excel file
-    try:
-        print(f"Writing dataframe with {len(df)} rows to Excel")
-        df.to_excel(output_excel_path, index=False)
-        print(f"Completed writing to Excel at {output_excel_path}")
-    except Exception as e:
-        print(f"Error writing to Excel file: {e}")
-
     # Close client to free resources
     client.close()
+    return df
 
 def main(args):
-    process_prompts(args.jsonl_path, args.output_excel_path, args.model, args.max_lines)
+    if not verify_paths(args.jsonl_path, args.output_excel_path):
+        print("One or more specified paths is incorrect. Please check the paths and try again.")
+        return
+
+    df = process_prompts(args.jsonl_path, args.model, args.max_lines)
+    write_df_to_excel(df, args.output_excel_path)
 
 
 if __name__ == "__main__":
